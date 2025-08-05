533b9eecfd20bce99708f53373a2b9ef422779fd
Ajay <gangwar030@gmail.com>
2025-08-05 06:02:21 +0530

    Opps Concept

--- OOPs/Call.js
+++ OOPs/Call.js
@@ -5,14 +5,15 @@ function user(username){
     
 }
 
-function Details(username, age, gender, work){
+function Details(username, age, gender, work, place){
     user.call(this, username)
     this.age = age;
     this.gender = gender;
     this.work = work;
+    this.place = place;
     console.log("This User Details will Print When function Will be Call");
 }
-let print = new Details("Pirate Carabian", 23, "Male", "Thief");
-console.log(print);
+let print = new Details("Pirate Carabian", 23, "Male", "Thief", "Ships");
+console.log("Here are Some Detail of User : ", print);
 
 
