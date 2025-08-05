62425cdcde98d0ee0e0d094ccc670e68d80c9eeb
Ajay <gangwar030@gmail.com>
2025-08-05 06:00:31 +0530

    Opps Concept

--- OOPs/Call.js
+++ OOPs/Call.js
@@ -10,6 +10,7 @@ function Details(username, age, gender, work){
     this.age = age;
     this.gender = gender;
     this.work = work;
+    console.log("This User Details will Print When function Will be Call");
 }
 let print = new Details("Pirate Carabian", 23, "Male", "Thief");
 console.log(print);
