71dfc09f17e3306dbff5bce498af1ccecf632c78
Ajay <gangwar030@gmail.com>
2025-08-07 10:59:54 +0530

    Constructor

---
+++ OOPs/Constructor.js
@@ -0,0 +1,35 @@
+class Student{
+
+    constructor(name, age, grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    fullname(surname){
+        return this.name + ' ' + surname;
+    }
+}
+
+let print = new Student("Ajay", 34, "A");
+console.log(print);
+
+console.log("My Name is ",print.fullname("Gangwar"));
+
+function user(username, age, email) {
+    this.username = username;
+    this.age = age;
+    this.email = email;
+}
+
+user.prototype.fullname = function(surname){
+    return this.username + ' ' + surname;
+
+}
+
+let printt = new user("Ajay", 34, "ajaygangwar@gmail.com");
+console.log(printt);
+
+console.log(printt.fullname("Gangwar"));
+
+
