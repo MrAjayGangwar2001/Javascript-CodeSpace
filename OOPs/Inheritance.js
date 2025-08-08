32ccf911eb86ca3b297c1ea34a6d30649b3ffed7
Ajay <gangwar030@gmail.com>
2025-08-08 09:10:08 +0530

    Inheritance1

---
+++ OOPs/Inheritance.js
@@ -0,0 +1,48 @@
+//   Here We are Going to Learn ProtoType Inheritance
+//   ProtoType Inheritance is a Old way of Inheritance in JavaScript
+//   Also We will See Modern Way of Inheritance in JavaScript
+
+
+//  Old Methods.............
+let user = {
+    name: "Ajay",
+    age: 24,
+    Academy : "UnCodeMy",
+    occupation: "Software Engineer"
+}
+
+const Trainer = {
+    Trainer_Name : "Snehank",
+    Traning_and_Guiding : true
+}
+
+const PlacementSupport = {
+    isSupport : false
+}
+
+const OfficeSupport = {
+    isAvailable24x7 : 'Not 24x7',
+    isSupport : true,
+  //  __proto__:PlacementSupport
+}
+
+Trainer__proto__= user                          // Yahan Teacher bhi User ki property access kar rha hai
+
+
+// Modern Way of Inheritance in JavaScript....-.--------- :-)
+
+
+Object.setPrototypeOf(Trainer, user); // Yahan Teacher bhi User ki property access kar rha hai
+
+
+
+
+let name = "      Ajay         "
+
+String.prototype.TrueLength = function(){
+    return this.trim().length
+}
+console.log(name.TrueLength())
+
+"        Gangwar  ".TrueLength();
+"    Java       Full      Stack    ".TrueLength();
