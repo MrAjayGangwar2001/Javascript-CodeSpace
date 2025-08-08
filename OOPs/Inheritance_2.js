65d6532e40b803c0e1898e3f6f9a8045df70fffb
Ajay <gangwar030@gmail.com>
2025-08-08 22:29:13 +0530

    Inheritance2.js

---
+++ OOPs/Inheritance_2.js
@@ -0,0 +1,33 @@
+
+
+class User{
+    constructor(name){
+        this.name = name;
+    }
+
+    print(){
+        console.log(this.name);
+    }
+}
+
+class Teacher extends User {
+    constructor(name, email, Address) {
+        super(name)
+
+        this.email = email;
+        this.Address = Address;
+    }
+
+    AddCourse(){
+        console.log(`Course Added by ${this.name}`);
+    }
+}
+
+let teacher = new Teacher("Ajay", "Ajay@example.com", "123 Main St.");
+console.log(teacher);
+// teacher.AddCourse();
+// teacher.print();
+
+// let student = new User("vivek")
+// student.print();
+
