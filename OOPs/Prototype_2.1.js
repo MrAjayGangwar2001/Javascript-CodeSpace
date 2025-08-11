1883fefe7cca3897468f8d83d3300bb07eba08cb
Ajay <gangwar030@gmail.com>
2025-08-11 06:18:21 +0530

    Prototype2

---
+++ OOPs/Prototype_2.1.js
@@ -0,0 +1,29 @@
+let Name = "      Ajay      ";
+let user = "      Rohan      ";
+
+String.prototype.TrueLength = function(){
+    return this.trim().length;
+}
+console.log(user.TrueLength())
+ console.log(Name.TrueLength());
+
+console.log(Name.trim().length);
+console.log(user.trim().length);
+
+
+let Actor = ["Akshay", "Sunil", "Paresh"];
+
+let ActorMovie = {
+    "Akshay" : "Garam Masala",
+    "Sunil" : "Dhamaal",
+    "Paresh" : "Golmaal",
+
+    ActorMixMovie : function(){
+        console.log(`Thes all three Actor Movie is ${this.paresh}`);
+        
+    }
+}
+
+Object.prototype.Ajay = function () {                      // It is Injecting Ajay FunctionUnder the Objects
+        console.log("Hello ! I am Ajay Under The Prototype of Object");
+}
