d128cd4c31deded10d0714a18875162b94ee1860
Ajay <gangwar030@gmail.com>
2025-08-16 08:05:29 +0530

    GetSetby Prop

--- GetSet_by_Prop.js
+++ GetSet_by_Prop.js
@@ -25,7 +25,7 @@ console.log(user.Email);
 console.log(user.Name);
 user.Email = 'NewEmail@example.com';
 user.Name  = "Vijay Malya";
-console.log(user.Email);
-console.log(user.Name);
+console.log("New Updated User Email", user.Email);
+console.log("New Updated User Name",user.Name);
 
 
