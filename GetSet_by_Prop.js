31a73dc4ecf6c0a82b1b8156a9ea0458cc67d9ba
Ajay <gangwar030@gmail.com>
2025-08-16 08:00:43 +0530

    GetSetby Prop

---
+++ GetSet_by_Prop.js
@@ -0,0 +1,25 @@
+function User(Name, Email){
+    this._Name = Name;
+    this._Email = Email;
+
+
+    Object.defineProperty(this, 'Email' ,{
+        get : function(){
+            return this._Email;
+        },
+        set : function(email){
+            this._Email = email;
+        }
+    })
+    Object.defineProperty(this, 'Name' ,{
+        get : function(){
+            return this._Name;
+        },
+        set : function(newName){
+            this._Name = newName;
+        }
+    })
+}
+var user = new User('John', 'john@example.com');
+console.log(user.Email);
+console.log(user.Name);
